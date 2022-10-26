302992d91ed52af58b864797e29ab985be19fafd
Rafael Gil <cybervoid@cybervoid.ga>
2022-10-26 17:00:04 -0400

    added a registration option

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -10,18 +11,23 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'blog_list')]
-    public function index(UserPasswordHasherInterface $passwordHasher)
+    public function index(UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine)
     {
         // ... e.g. get the user data from a registration form
         $user              = new User();
-        $plaintextPassword = "";
+        $plaintextPassword = "rafa";
+        $em                = $doctrine->getManager();
 
         // hash the password (based on the security.yaml config for the $user class)
         $hashedPassword = $passwordHasher->hashPassword(
             $user,
             $plaintextPassword
         );
+
         $user->setPassword($hashedPassword);
+        $user->setEmail("rgil@rectanglehealth.com");
+        $em->persist($user);
+        $em->flush($user);
 
         return $this->redirect("/");
     }
